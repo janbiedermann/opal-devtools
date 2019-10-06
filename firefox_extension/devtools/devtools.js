bb177f9970b3ee5756a4b87ee8f331e30352f382
Jan Biedermann <jan@kursator.de>
2019-10-06 11:33:45 +0200

    Check for Opal on navigation.

--- firefox_extension/devtools/devtools.js
+++ firefox_extension/devtools/devtools.js
@@ -5,14 +5,37 @@ This script is run whenever the devtools are open.
 In here, we can create our panel.
 */
 
+function check_page_for_opal() {
+    chrome.devtools.inspectedWindow.eval(
+        `var framework = null;
+        var opal_version = null;
+        var devtools_support = false;
+        if (typeof Opal !== "undefined") {
+            opal_version = Opal.RUBY_ENGINE_VERSION;
+            if (typeof Opal.Isomorfeus !== "undefined") { framework = "isomorfeus" }
+            else if (typeof Opal.Hyperstack !== "undefined") { framework = "hyperstack" }
+            else if (typeof Opal.Hyperloop !== "undefined") { framework = "hyperloop" }
+            else if (typeof Opal.Clearwater !== "undefined") { framework = "clearwater" }
+            if (typeof Opal.opal_devtools_object_registry !== "undefined") { devtools_support = true }
+        }
+        [opal_version, framework, devtools_support]`,
+        {},
+        function(res, error) {
+            let event = new CustomEvent('OpalDevtoolsPageCheck', { detail: { opal_version: res[0], framework: res[1], devtools_support: res[2] }});
+            panel_window.dispatchEvent(event);
+        });
+};
+
 let panel_window = null;
 
 function handleShown(p_window) {
   panel_window = p_window;
+  check_page_for_opal();
+  chrome.devtools.network.onNavigated.addListener(check_page_for_opal);
 }
 
 function handleHidden() {
-  // console.log("panel is being hidden");
+  chrome.devtools.network.onNavigated.removeListener(check_page_for_opal);
 }
 
 /**
@@ -38,4 +61,3 @@ chrome.devtools.panels.create(
   });
   chrome.runtime.connect({name: "opal-devtools-page"});
 });
-
