58239d71d9655f29db3a115a1cb93fab8f10a837
Jan Biedermann <jan@kursator.de>
2019-10-11 11:25:13 +0200

    Update build script.

--- build_zips.sh
+++ build_zips.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+rm -f chrome_extension/devtools/panel/*.js
+rm -f firefox_extension/devtools/panel/*.js
 rm -f chrome_extension/devtools/panel/*.map
 rm -f firefox_extension/devtools/panel/*.map
 rm *.zip
+
+yarn run production_build
 
 cd chrome_extension
 zip -r ../opal_devtools_chrome.zip *
